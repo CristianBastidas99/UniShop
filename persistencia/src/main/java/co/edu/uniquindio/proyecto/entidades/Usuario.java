221de3efb6ac550974bbae91e74fb140121a75f6
Cristian Bastidas <cfbastidaso@uqviertual.edu.co>
2021-10-14 16:02:41 -0500

    Se empieza con las entidades

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Usuario.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Usuario.java
@@ -1,7 +1,37 @@
 package co.edu.uniquindio.proyecto.entidades;
 
-public class Usuario {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+public class Usuario implements Serializable{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nombreCompleto;
+
+    private String email;
+
+    private String contraseña;
+
+    @ElementCollection
+    private List<String> numeroTelefono;
+
+    @ElementCollection
+    private Map<String, String> ubicacion;
 
 
-
+    public Usuario( String nombreCompleto, String email, String contraseña) {
+        this.id = id;
+        this.nombreCompleto = nombreCompleto;
+        this.email = email;
+        this.contraseña = contraseña;
+        this.numeroTelefono = null;
+        this.ubicacion = null;
+    }
 }
