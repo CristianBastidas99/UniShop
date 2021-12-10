ae0e99ffa80d692bc5ba44bb399dbacccb281b08
Cristian Bastidas <cfbastidaso@uqviertual.edu.co>
2021-12-10 11:52:27 -0500

    se actualizan xhtml

---
+++ persistencia/target/test-classes/usuarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO Usuario (id, nombre, email, password, id_ciudad) VALUES (1, "Lorena Lopez", "lorena@email.com", "12345", "1");
+INSERT INTO Usuario (id, nombre, email, password, id_ciudad) VALUES (2, "Marta Wayne", "marta@email.com", "12345", "4");
+INSERT INTO Usuario (id, nombre, email, password, id_ciudad) VALUES (3, "Pepe Perez", "pepe@email.com", "12345", "2");
+INSERT INTO Usuario (id, nombre, email, password, id_ciudad) VALUES (4, "Pedro Martinez", "pedro@email.com", "12345", "3");
